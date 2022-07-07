2b932a4bbdb03ba9ee15147dc8e72d4fabe413da
Maciejw52 <mwyszogrodzki52@gmail.com>
2022-07-07 11:18:58 +0100

    Initialise react app

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,31 @@
 import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
   // My configs
 }
 
 const app = initializeApp(firebaseConfig);
+
+
+export async function loginWithGoogleAccount() {
+  // returns user data if sign in successful
+  try {
+    const provider = newGoogleAuthProvider();
+    
+    const { user } = await signInWithPopup(getAuth(), provider);
+
+    console.log(user);
+
+    return {
+      uid: user.uid,
+      displayName: user.displayName
+    }
+
+  } catch (err) {
+    if(err.code !== "auth/cancelled-popul-request"){
+      console.error(err);
+    }
+  }
+}
